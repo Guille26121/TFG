8a31fc890b3f9e701a9d5d2d6cce86db75e8553e
Guille26121 <guille26121yutu@gmail.com>
2023-10-05 10:20:52 +0200

    archivos movidos de sitio

--- Facile_J/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module Test_FX_n4 {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires java.desktop;
-	requires javafx.base;
-	requires javafx.graphics;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
